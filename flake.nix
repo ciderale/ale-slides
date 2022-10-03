9ee14fec590cd521153dfaaab051ba00510fb438
Alain Lehmann <alain.lehmann@gmail.com>
2022-10-03 23:24:41 +0200

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -8,26 +8,36 @@
     revealJs.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, revealJs }: {
-    homeManagerModule = { pkgs, ... }: {
-      nixpkgs.overlays = [self.overlay];
-      home.packages = with pkgs.ale-slides; [
-        slides-init slides-preview slides-build
-      ];
-    };
-
-    overlay = final: prev: {
-      ale-slides = prev.pkgs.callPackages ./default.nix {
-        inherit revealJs;
-        inherit (prev.nodePackages) browser-sync;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    revealJs,
+  }:
+    {
+      homeManagerModule = {pkgs, ...}: {
+        nixpkgs.overlays = [self.overlay];
+        home.packages = with pkgs.ale-slides; [
+          slides-init
+          slides-preview
+          slides-build
+        ];
       };
-    };
 
-  } // flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs { inherit system; overlays = [self.overlay]; };
-  in {
-
-    packages = pkgs.ale-slides;
-
-  });
+      overlay = final: prev: {
+        ale-slides = prev.pkgs.callPackages ./default.nix {
+          inherit revealJs;
+          inherit (prev.nodePackages) browser-sync;
+        };
+      };
+    }
+    // flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [self.overlay];
+      };
+    in {
+      packages = pkgs.ale-slides;
+      formatter = pkgs.alejandra;
+    });
 }
